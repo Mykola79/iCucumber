5e07560a494f02385c7e75ab8682034d5e4d65e7
Mykola79 <mykola.rusyn79@gmail.com>
2023-04-04 22:12:14 -0400

    added employee

--- src/test/java/TestRunner/SmokeRunner.java
+++ src/test/java/TestRunner/SmokeRunner.java
@@ -6,6 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src\\test\\resources\\Features",glue = "StepDefinitions",dryRun = false)
+@CucumberOptions(features = "src\\test\\resources\\Features",glue = "StepDefinitions",dryRun = false,tags = "@testcase2")
 public class SmokeRunner {
 }
+//tags option will execute the tagged testcase as mentioned
--- target/test-classes/Features/Login.feature
+++ target/test-classes/Features/Login.feature
@@ -1,7 +1,9 @@
 Feature: Login Functionalities
+
+  @testcase1
   Scenario: Valid Admin login
     Given open the browser and launch HRMS application
     When user enters valid email and valid password
     And click on login button
     Then user is logged in successfully into application
-    And close the browser
+    #And close the browser
